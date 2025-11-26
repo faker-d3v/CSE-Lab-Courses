aa18b23a24fd6609f1a0d6d1753e8bafd423e809
johnny.dang <johnny.dang@rey.gg>
2025-11-26 19:14:47 +0600

    DSA with syntactic sugar 🍰

--- 3rd/DSA-Lab/task-1/linear-search.cpp
+++ 3rd/DSA-Lab/task-1/linear-search.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 
 int main() {
-  const int n = 10;
-  int arr[n] = {7, 2, 10, 1, 5, 9, 4, 6, 8, 3};
-  int target = 9;
+    const int n = 10;
+    int arr[n] = {7, 2, 10, 1, 5, 9, 4, 6, 8, 3};
+    int target = 9;
 
-  bool found = false;
-  for (int i = 0; i < n; i++) {
-    if (arr[i] == target) {
-      found = true;
-      break;
+    bool found = false;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            found = true;
+            break;
+        }
     }
-  }
 
-  if (found) {
-    cout << "Found" << endl;
-    return 0;
-  }
+    if (found) {
+        cout << "Found" << endl;
+        return 0;
+    }
 
-  cout << "Not found" << endl;
+    cout << "Not found :(" << endl;
 }
